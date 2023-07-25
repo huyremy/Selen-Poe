a2e549f0191942ff79fb9c839f1ed19e0aba69ad
Nguyễn Quang Huy <huynq@isi.com.vn>
2023-07-25 12:27:22 +0700

    Create botass.py

---
+++ botass.py
@@ -0,0 +1,18 @@
+import random
+import time
+
+from selenium.common.exceptions import TimeoutException, ElementNotInteractableException
+from selenium.webdriver.common.by import By
+
+def login(self):
+    """Login to Path of Exil - POE"""
+    try:
+        self.browser.get("https://poe.com/login?redirect_url=%2F")
+        time.sleep(random.uniform(5, 10))
+        self.browser.find_element_by_id("EmailInput_emailInput__4v_bn").send_keys(self.email)
+        self.browser.find_element_by_css_selector("Button_buttonBase__0QP_m Button_primary__pIDjn").click()
+        self.browser.find_element_by_id("VerificationCodeInput_verificationCodeInput__YD3KV").send_keys(self.password)
+        self.browser.find_element_by_css_selector("Button_buttonBase__0QP_m Button_primary__pIDjn").click()
+        time.sleep(random.uniform(5, 10))
+    except TimeoutException:
+        raise Exception("Could not login!")
