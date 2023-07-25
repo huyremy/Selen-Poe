a520d541aa0d9dfc9f095f0f5cc0eb284dd1eb3a
Nguyễn Quang Huy <huynq@isi.com.vn>
2023-07-25 12:30:02 +0700

    Update botass.py

--- botass.py
+++ botass.py
@@ -7,7 +7,8 @@ from selenium.webdriver.common.by import By
 def login(self):
     """Login to Path of Exil - POE"""
     try:
-        self.browser.get("https://poe.com/login?redirect_url=%2F")
+        self.browser.get("https://HIDDEN-URL.com/login?redirect_url=%2F")URL
+        # HIDDEN-URL for security change it to your login domain
         time.sleep(random.uniform(5, 10))
         self.browser.find_element_by_id("EmailInput_emailInput__4v_bn").send_keys(self.email)
         self.browser.find_element_by_css_selector("Button_buttonBase__0QP_m Button_primary__pIDjn").click()
